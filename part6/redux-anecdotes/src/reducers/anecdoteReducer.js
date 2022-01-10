8ddfa48d9e30c3ce2249c178994814c6e4851493
Ossian Rajala <ossian.rajala@gmail.com>
2022-01-10 12:56:52 +0200

    6.13: fetch anecdotes from json-server

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,9 +8,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_NEW': {
       return [...state, asObject(action.data.content)]
@@ -32,6 +21,9 @@ const anecdoteReducer = (state = initialState, action) => {
         votes: anecdote.votes + 1,
       }
       return state.map((a) => (a.id !== id ? a : updatedAnecdote))
+    }
+    case 'INIT_DATA': {
+      return action.data
     }
     default:
       return state
@@ -49,6 +41,13 @@ export const createAnecdote = (content) => {
   return {
     type: 'ADD_NEW',
     data: { content },
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_DATA',
+    data: anecdotes,
   }
 }
 
