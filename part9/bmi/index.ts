7d7351979502b5b56b1ad3da6d8e170280246051
Ossian Rajala <ossian.rajala@gmail.com>
2022-01-22 20:40:30 +0200

    9.5: endpoint for bmi calculation

--- part9/bmi/index.ts
+++ part9/bmi/index.ts
@@ -1,11 +1,32 @@
 import express from 'express';
 const app = express();
 
+import { calculateBmi } from './bmiCalculator';
+
+app.get('/', (_req, res) => {
+  res.send('Server is up and running!');
+});
+
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const { weight, height } = req.query;
+
+  if (!isNaN(Number(weight)) && !isNaN(Number(height))) {
+    const bmi = calculateBmi(Number(height), Number(weight));
+    res.send({
+      weight: Number(weight),
+      height: Number(height),
+      bmi: bmi,
+    });
+  } else {
+    res.send({ error: 'malformatted parameters' });
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
