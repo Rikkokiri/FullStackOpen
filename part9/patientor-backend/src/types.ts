009fafcdb9e155f6588d43b0e3803d783d9d216e
Ossian Rajala <ossian.rajala@gmail.com>
2022-01-30 08:01:23 +0200

    9.11: patients endpoint

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -3,3 +3,13 @@ export interface Diagnose {
   name: string;
   lating?: string;
 }
+export interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+}
+
+export type PublicPatient = Omit<Patient, 'ssn'>;
