6397e618d285edaf7938509ffb01922ae417b303
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-13 13:07:16 +0300

    feat(5.1): implement login and logout

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,17 +1,75 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
-import blogService from './services/blogs';
+import * as blogService from './services/blogs';
+import * as loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log('Logging in');
+
+    try {
+      const user = await loginService.login({ username, password });
+      console.log('login response', user);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <h1>Log in to the application</h1>
+        <div>
+          <label htmlFor="username">username </label>
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+            name="username"
+            id="username"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password </label>
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            name="password"
+            id="password"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+  };
+
+  if (!user) {
+    return loginForm();
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
+      <p>{user.name || user.username} logged in</p>{' '}
+      <button onClick={handleLogout}>Log out</button>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
