d4abc7ee846f0bfa0692505d46bda68cbb675dc6
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-19 09:48:20 +0300

    feat(5.9): sort posts by likes

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -116,9 +116,11 @@ const App = () => {
         <BlogForm createBlog={createBlog} />
       </Togglable>
       <br />
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+        ))}
     </div>
   );
 };
