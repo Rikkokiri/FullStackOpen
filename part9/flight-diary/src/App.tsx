6a3b89b9c0d51f5c2dea2f479fdc12ed1500dcc2
Ossian Rajala <ossian.rajala@gmail.com>
2023-04-10 13:23:04 +0300

    ex 9.16: add new flight entries (no validation)

--- part9/flight-diary/src/App.tsx
+++ part9/flight-diary/src/App.tsx
@@ -1,16 +1,79 @@
 import { useEffect, useState } from 'react';
-import { DiaryEntry } from './types';
-import { getAllDiaryEntries } from './diaryService';
+import { DiaryEntry, NewDiaryEntry } from './types';
+import { createDiaryEntry, getAllDiaryEntries } from './diaryService';
 
 const App = () => {
   const [entries, setEntries] = useState<DiaryEntry[]>();
+  const [date, setDate] = useState<string>('');
+  const [visibility, setVisibility] = useState<string>('');
+  const [weather, setWeather] = useState<string>('');
+  const [comment, setComment] = useState<string>('');
+
   useEffect(() => {
     getAllDiaryEntries().then((data: DiaryEntry[]) => setEntries(data));
   }, []);
 
+  const createEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const newEntry: NewDiaryEntry = {
+      date: date,
+      visibility: visibility,
+      weather: weather,
+      comment: comment,
+    };
+
+    createDiaryEntry(newEntry).then((data: DiaryEntry) => {
+      setEntries(entries?.concat(data));
+    });
+
+    setDate('');
+    setVisibility('');
+    setWeather('');
+    setComment('');
+  };
+
   return (
     <main>
       <h1>Flight Diary</h1>
+      <form onSubmit={createEntry}>
+        <h2>Add new entry</h2>
+        <div className="input-wrapper">
+          <label htmlFor="entry-date">Date</label>
+          <input
+            id="entry-date"
+            type="date"
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          ></input>
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="visibility">Visibility</label>
+          <input
+            id="visibility"
+            type="text"
+            value={visibility}
+            onChange={(event) => setVisibility(event.target.value)}
+          ></input>
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="weather">Weather</label>
+          <input
+            id="weather"
+            type="text"
+            value={weather}
+            onChange={(event) => setWeather(event.target.value)}
+          ></input>
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="comment">Comment</label>
+          <textarea
+            id="comment"
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit">Add</button>
+      </form>
       <section>
         <h2>Diary entries</h2>
         <ul className="entry-list">
--- part9/flight-diary/src/diaryService.ts
+++ part9/flight-diary/src/diaryService.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
-import { DiaryEntry } from './types';
+import { DiaryEntry, NewDiaryEntry } from './types';
 
 const baseUrl = 'http://localhost:3001/api/diaries';
 
 export const getAllDiaryEntries = () => {
   return axios.get<DiaryEntry[]>(baseUrl).then((response) => response.data);
+};
+
+export const createDiaryEntry = async (entry: NewDiaryEntry) => {
+  const response = await axios.post(baseUrl, entry);
+  return response.data;
 };
